4ca704e116ef3af388623ad2b4a3682587e04426
Erik Torres <erik.torres@lifullconnect.com>
2022-08-25 09:37:01 +0200

    :white_check_mark: Avro round trip is a test helper

--- libs/commons-lang/src/test/scala/shared/infrastructure/AvroRoundTripSuite.scala
+++ libs/commons-lang/src/test/scala/shared/infrastructure/AvroRoundTripSuite.scala
@@ -1,20 +1,22 @@
 package es.eriktorr.library
 package shared.infrastructure
 
+import munit.Assertions.assertEquals
 import munit.ScalaCheckSuite
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Gen, Prop}
 import vulcan.Codec
 
-object AvroRoundTripSuite extends ScalaCheckSuite:
+object AvroRoundTripSuite:
 
-  def checkUsing[A](genA: Gen[A], codecA: Codec[A]): Prop = forAll(genA) { a =>
-    assertEquals(
-      for
-        schema <- codecA.schema
-        payload <- codecA.encode(a)
-        result <- codecA.decode(payload, schema)
-      yield result,
-      Right(a),
-    )
-  }
+  def checkUsing[A](genA: Gen[A], codecA: Codec[A])(implicit ev: Unit => Prop): Prop =
+    forAll(genA) { a =>
+      assertEquals(
+        for
+          schema <- codecA.schema
+          payload <- codecA.encode(a)
+          result <- codecA.decode(payload, schema)
+        yield result,
+        Right(a),
+      )
+    }
