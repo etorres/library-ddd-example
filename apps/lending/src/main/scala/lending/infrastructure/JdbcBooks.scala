dbd3f7f6059bd6ebd927df2ec32a90e4751a9f55
Erik Torres <erik.torres@lifullconnect.com>
2022-09-01 08:47:08 +0200

    :art: Move smart constructors to infrastructure layer

--- apps/lending/src/main/scala/lending/infrastructure/JdbcBooks.scala
+++ apps/lending/src/main/scala/lending/infrastructure/JdbcBooks.scala
@@ -8,9 +8,12 @@ import lending.infrastructure.LibraryBranchIdJdbcMapping
 import lending.model.*
 import lending.model.Book.{AvailableBook, BookOnHold, CheckedOutBook}
 import lending.model.BookState.{Available, CheckedOut, OnHold}
+import shared.ValidationError
+import shared.validated.AllErrorsOr
 import shared.validated.ValidatedIO.validatedNecIO
 
 import cats.effect.IO
+import cats.syntax.all.*
 import doobie.Transactor
 import doobie.implicits.*
 import doobie.postgres.implicits.*
@@ -37,7 +40,7 @@ final class JdbcBooks(transactor: Transactor[IO])
           $bookType,
           ${BookState.Available},
           $libraryBranchId
-        )""".stripMargin.update.run.transact(transactor)
+        )""".update.run.transact(transactor)
     case _ => IO.unit
 
   override def findBy(bookId: BookId): IO[Option[Book]] = sql"""
@@ -52,37 +55,23 @@ final class JdbcBooks(transactor: Transactor[IO])
         checked_out_by_patron,
         on_hold_till
       FROM books
-      WHERE book_id = $bookId""".stripMargin
+      WHERE book_id = $bookId"""
     .query[RawBook]
     .option
     .transact(transactor)
     .flatMap {
       case Some(rawBook) =>
         (rawBook.bookState match
-          case Available =>
-            AvailableBook.from(rawBook.bookId, rawBook.bookType, rawBook.availableAtBranch)
-          case CheckedOut =>
-            CheckedOutBook
-              .from(
-                rawBook.bookId,
-                rawBook.bookType,
-                rawBook.checkedOutAtBranch,
-                rawBook.checkedOutByPatron,
-              )
-          case OnHold =>
-            BookOnHold
-              .from(
-                rawBook.bookId,
-                rawBook.bookType,
-                rawBook.onHoldAtBranch,
-                rawBook.onHoldByPatron,
-                rawBook.onHoldTill,
-              )
+          case Available => rawBook.asAvailableBook
+          case CheckedOut => rawBook.asCheckedOutBook
+          case OnHold => rawBook.asBookOnHold
         ).validated.map(Some.apply)
       case None => IO.pure(None)
     }
 
 object JdbcBooks:
+  final case class InvalidDatabaseEntity(message: String) extends ValidationError(message)
+
   final case class RawBook(
       bookId: BookId,
       bookType: BookType,
@@ -93,4 +82,39 @@ object JdbcBooks:
       checkedOutAtBranch: Option[LibraryBranchId],
       checkedOutByPatron: Option[PatronId],
       onHoldTill: Option[Instant],
-  )
+  ):
+    def asAvailableBook: AllErrorsOr[AvailableBook] =
+      (
+        bookId.validNec,
+        bookType.validNec,
+        availableAtBranch.fold(
+          InvalidDatabaseEntity(s"Missing library branch Id for available book: $bookId").invalidNec,
+        )(_.validNec),
+      ).mapN(AvailableBook.apply)
+
+    def asBookOnHold: AllErrorsOr[BookOnHold] = (
+      bookId.validNec,
+      bookType.validNec,
+      onHoldAtBranch.fold(
+        InvalidDatabaseEntity(s"Missing library branch for book on hold: $bookId").invalidNec,
+      )(_.validNec),
+      onHoldByPatron.fold(
+        InvalidDatabaseEntity(s"Missing patron for book on hold: $bookId").invalidNec,
+      )(
+        _.validNec,
+      ),
+      onHoldTill.validNec,
+    ).mapN(BookOnHold.apply)
+
+    def asCheckedOutBook: AllErrorsOr[CheckedOutBook] = (
+      bookId.validNec,
+      bookType.validNec,
+      checkedOutAtBranch.fold(
+        InvalidDatabaseEntity(s"Missing library branch for checked out book: $bookId").invalidNec,
+      )(_.validNec),
+      checkedOutByPatron.fold(
+        InvalidDatabaseEntity(s"Missing patron for checked out book: $bookId").invalidNec,
+      )(
+        _.validNec,
+      ),
+    ).mapN(CheckedOutBook.apply)
--- apps/lending/src/main/scala/lending/model/Book.scala
+++ apps/lending/src/main/scala/lending/model/Book.scala
@@ -2,10 +2,6 @@ package es.eriktorr.library
 package lending.model
 
 import book.model.{BookId, BookInstance, BookType}
-import shared.ValidationError
-import shared.validated.AllErrorsOr
-
-import cats.syntax.all.*
 
 import java.time.Instant
 
@@ -21,19 +17,6 @@ object Book:
   ) extends Book
 
   object AvailableBook:
-    def from(
-        bookId: BookId,
-        bookType: BookType,
-        maybeLibraryBranchId: Option[LibraryBranchId],
-    ): AllErrorsOr[AvailableBook] =
-      (
-        bookId.validNec,
-        bookType.validNec,
-        maybeLibraryBranchId.fold(
-          InvalidBook(s"Missing library branch Id for available book: $bookId").invalidNec,
-        )(_.validNec),
-      ).mapN(AvailableBook.apply)
-
     def from(bookInstance: BookInstance, libraryBranchId: LibraryBranchId): AvailableBook =
       AvailableBook(bookInstance.bookId, bookInstance.bookType, libraryBranchId)
 
@@ -45,48 +28,9 @@ object Book:
       holdTill: Option[Instant],
   ) extends Book
 
-  object BookOnHold:
-    def from(
-        bookId: BookId,
-        bookType: BookType,
-        maybeHoldPlacedAt: Option[LibraryBranchId],
-        maybeByPatron: Option[PatronId],
-        holdTill: Option[Instant],
-    ): AllErrorsOr[BookOnHold] = (
-      bookId.validNec,
-      bookType.validNec,
-      maybeHoldPlacedAt.fold(
-        InvalidBook(s"Missing library branch for book on hold: $bookId").invalidNec,
-      )(_.validNec),
-      maybeByPatron.fold(InvalidBook(s"Missing patron for book on hold: $bookId").invalidNec)(
-        _.validNec,
-      ),
-      holdTill.validNec,
-    ).mapN(BookOnHold.apply)
-
   final case class CheckedOutBook(
       bookId: BookId,
       bookType: BookType,
       checkedOutAt: LibraryBranchId,
       byPatron: PatronId,
   ) extends Book
-
-  object CheckedOutBook:
-    def from(
-        bookId: BookId,
-        bookType: BookType,
-        maybeCheckedOutAt: Option[LibraryBranchId],
-        maybeByPatron: Option[PatronId],
-    ): AllErrorsOr[CheckedOutBook] =
-      (
-        bookId.validNec,
-        bookType.validNec,
-        maybeCheckedOutAt.fold(
-          InvalidBook(s"Missing library branch for checked out book: $bookId").invalidNec,
-        )(_.validNec),
-        maybeByPatron.fold(InvalidBook(s"Missing patron for checked out book: $bookId").invalidNec)(
-          _.validNec,
-        ),
-      ).mapN(CheckedOutBook.apply)
-
-  final case class InvalidBook(message: String) extends ValidationError(message)
